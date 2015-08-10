b4469a376e0f10ebfd02d529e418eddd73368c31
Ike Mattice & Deron Johnson <student@epicodus.com>
2015-08-10 16:09:01 -0700

    Display up to 3

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -6,8 +6,13 @@
             $count = 1;
             $result = array();
             while ($count <= $input) {
+                if ($count % 3 == 0) {
+                    array_push($result, "Ping");
+                } else {
             array_push($result, /*strval($count)*/ $count);
+                }
                 ++$count;
+
             }
             return $result;
         }
