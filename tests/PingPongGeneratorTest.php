97c165ae4c2da604288d952c90d5dfa4de61136b
Ike Mattice & Deron Johnson <student@epicodus.com>
2015-08-10 16:11:57 -0700

    Add test for five.

--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -51,9 +51,22 @@
             //Assert
             $this->assertEquals(array(1, 2, "Ping"), $result);
         }
+
         //User enters 5
         //input -> 5
         //output -> 1 2 ping 4 pong
+        function test_generatePingPongArray_five()
+        {
+            //Arrange
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input = 5;
+
+            //Act
+            $result = $test_PingPongGenerator->generatePingPongArray($input);
+
+            //Assert
+            $this->assertEquals(array(1, 2, "Ping", 4, "Pong"), $result);
+        }
 
         //User enters 15
         //input -> 15
